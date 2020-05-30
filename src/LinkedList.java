c6c26b1a908d0858457f925818ffaf024aa9235d
Wr2020 <comin_008@163.com>
2020-05-30 21:23:36 +0800

    create linkedlist

--- src/LinkedList.java
+++ src/LinkedList.java
@@ -24,4 +24,74 @@ public class LinkedList<E> {
     }
 
 
+    private Node head;
+    private int size;
+
+    public LinkedList() {
+        head = null;
+        size = 0;
+    }
+
+    public int getSize() {
+        return size;
+    }
+
+    public boolean isEmpty() {
+        return size == 0;
+    }
+
+    /**
+     * 在链表头添加新元素e
+     *
+     * @param e
+     */
+    public void addFirst(E e) {
+
+//        Node node = new Node(e);
+//        node.next = head;
+//        head = node;
+
+        head = new Node(e, head);
+        size++;
+
+    }
+
+    /**
+     * 在链表的index(0-based)位置添加新的元素e
+     * 在链表中不是一个常用的操作，练习用
+     *
+     * @param index 索引
+     * @param e     添加的元素
+     */
+    @Deprecated
+    public void add(int index, E e) {
+
+        if (index < 0 || index > size) {
+            throw new IllegalArgumentException("Add failed. Illegal index.");
+        }
+
+        if (index == 0) {
+            addFirst(e);
+        } else {
+
+            Node prev = head;
+            for (int i = 0; i < index - 1; i++) {
+                prev = prev.next;
+            }
+//            Node node = new Node(e);
+//            node.next = prev.next;
+//            prev.next = node;
+
+            prev.next = new Node(e, prev.next);
+            size++;
+        }
+
+
+
+
+    }
+
+    public void addLast(E e) {
+        add(size, e);
+    }
 }
